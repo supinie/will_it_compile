8daca0ab222f0b70b6058ff2012b3311dd68b230
supinie <86788874+supinie@users.noreply.github.com>
2024-03-20 10:05:10 +0000

    clean up printing

--- src/unsafe_printing.rs
+++ src/unsafe_printing.rs
@@ -1,21 +1,32 @@
 pub unsafe fn unsafe_print_bool_vec(vec: Vec<bool>) {
-    for elem in vec {
+    print!("[");
+    for (i, elem) in vec.iter().enumerate() {
+        if i != 0 {
+            print!(", ");
+        }
         match elem {
-            true => println!("on god"),
-            false => println!("kapp"),
+            true => print!("on god"),
+            false => print!("kapp"),
         }
     }
+    print!("]\n");
 }
 
 pub fn unsafe_print_int_vec(mut vec: Vec<i16>) {
-    for chunk in vec.chunks_mut(2) {
+    print!("[");
+    let length = vec.len();
+    for (i, chunk) in vec.chunks_mut(2).enumerate() {
         let r1 = &chunk[0] as * const i16;
         let r2 = &mut chunk[1] as *mut i16;
 
         unsafe {
-            println!("{}", *r1);
-            println!("{}", *r2);
+            print!("{}, ", *r1);
+            print!("{}", *r2);
+        }
+        if i != (length / 2) - 1 {
+            print!(", ");
         }
     }
+    print!("]\n");
 }
 
